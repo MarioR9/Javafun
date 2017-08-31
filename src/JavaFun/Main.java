e07b14ee911630e10b51ca95e8591c025c1f6b6c
Rodriguez <mr20620@email.vccs.edu>
2017-08-31 17:22:18 -0400

    Initial commit

---
+++ src/JavaFun/Main.java
@@ -0,0 +1,14 @@
+package JavaFun;
+
+/**
+ * 
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("I love JAVA. JAVA is fun.");
+    }
+}
